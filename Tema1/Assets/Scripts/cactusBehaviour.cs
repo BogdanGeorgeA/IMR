7550b67d4de665745a52ca1a95ee2db2f7a8ed60
BogdanGeorgeA <61452935+BogdanGeorgeA@users.noreply.github.com>
2020-10-07 12:15:46 +0300

    Tema1
    Bogdan George-Alexandru
    Popa Stefan-Dorin

---
+++ Tema1/Assets/Scripts/cactusBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cactusBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Vector3.Distance(Camera.main.transform.position, transform.position) <= 0.5)
+        {
+            GetComponent<Animator>().SetBool("cameraClose", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("cameraClose", false);
+        }
+    }
+}
